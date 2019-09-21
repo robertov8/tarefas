497353dbfcb45bc75eed1510666900e4dedeb13a
Roberto Ribeiro <robertov82008@gmail.com>
2019-09-21 15:35:31 -0300

    151. Componente Lista de Tarefas #01

---
+++ src/components/Task.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="task" :class="stateClass">
+        {{ task.name }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Task',
+        props: {
+            task: { type: Object, required: true }
+        },
+        computed: {
+            stateClass() {
+                return {
+                    pending: this.task.pending,
+                    done: !this.task.pending
+                };
+            }
+        }
+    };
+</script>
+
+<style scoped>
+    .task {
+        box-sizing: border-box;
+        height: 150px;
+        width: 350px;
+        padding: 10px;
+        border-radius: 8px;
+        font-size: 2rem;
+        font-weight: 300;
+        cursor: pointer;
+        user-select: none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .pending {
+        border-left: 12px solid #b73229;
+        background-color: #f44336;
+    }
+
+    .done {
+        color: #ddd;
+        border-left: 12px solid #0a8f08;
+        background-color: #4caf50;
+        text-decoration: line-through;
+    }
+</style>
